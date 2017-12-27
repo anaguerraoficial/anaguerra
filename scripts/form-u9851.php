776e2abddd304f7483425f2e57eb4547d6381449
anagueraoficial <anawar@anawar.es>
2017-12-27 13:24:21 +0000

    Ana Guerra

--- scripts/form-u9851.php
+++ scripts/form-u9851.php
@@ -27,8 +27,8 @@ $form = array(
 		'unknown_method' => 'Unknown server request method'
 	),
 	'email' => array(
-		'from' => 'anaguerraweb@hotmail.com',
-		'to' => 'anaguerraweb@hotmail.com'
+		'from' => 'anawar@anawar.es',
+		'to' => 'anawar@anawar.es'
 	),
 	'fields' => array(
 		'custom_U9862' => array(
